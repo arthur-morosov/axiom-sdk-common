60755d48f879ed87eb27698a115d1b07c04d361a
amorozov <amorozov@accesssoftek.com>
2026-02-24 11:37:54 +0300

    initial common library

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,21 +11,52 @@ android {
     compileSdk = 36 // Укажите актуальную версию
     defaultConfig {
         minSdk = 24
+//        consumerProguardFiles("consumer-rules.pro") // Правила ProGuard
+    }
+
+    // Настройка публикации AAR
+    publishing {
+        singleVariant("release") {
+//            withSourcesJar() // Исходники (опционально)
+//            withJavadocJar() // Документация (опционально)
+        }
     }
 }
 
 //// Публикация артефакта
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                // Если вы публикуете AAR напрямую
-                artifact("library/common-release.aar")
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "com.github.arthur-morosov"  // Ваш GitHub username или организация
+            artifactId = "axiom-sdk-common"        // Имя артефакта
+            version = "0.0.3"                      // Версия релиза
 
-                groupId = "com.github.arthur-morosov"
-                artifactId = "axiom-sdk-common"
-                version = "0.0.2"
-            }
+            artifact("library/common-release.aar")
+//            from(components["common-release.aar"])            // Берет скомпилированный AAR
+
+            // Метаданные (для Maven Central, для GitHub опционально)
+//            pom {
+//                name.set("Axiom SDK Common")
+//                description.set("Common utilities for Axiom SDK")
+//                url.set("https://github.com/arthur-morosov/axiom-sdk-common")
+//                licenses {
+//                    license {
+//                        name.set("Proprietary")
+//                        url.set("https://example.com/license")
+//                    }
+//                }
+//            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/arthur-morosov/axiom-sdk-common")
+//            credentials {
+//                username = System.getenv("GITHUB_ACTOR")
+//                password = System.getenv("GITHUB_TOKEN")
+//            }
         }
     }
 }
